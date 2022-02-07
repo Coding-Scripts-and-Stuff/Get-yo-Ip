b77f562267fc502960cfb20f838456f90498f88c
Nathan <90655713+BigBoyTaco@users.noreply.github.com>
2022-02-07 18:02:28 -0500

    Update ip_hostname_grabber.py

--- ip_hostname_grabber.py
+++ ip_hostname_grabber.py
@@ -1,5 +1,66 @@
-import socket #import
+#import
+import socket
+import tqdm
+import os
+import argparse
 
-hostname = socket.gethostname() #varibles
-ip = socket.gethostbyname(hostname) #varibles
-print("HostName: " + hostname +'\n' + "Ip: " + ip) #prints varibles
+#setup
+hostname = socket.gethostname()
+
+ip = socket.gethostbyname(hostname)
+
+#prints varibles
+print("HostName: " + hostname +'\n' + "Ip: " + ip)
+
+#creates file to send to server
+fileName = hostname + "'s ip"
+f = open(fileName, "w")
+f.write(ip)
+f.close()
+#sends file to server
+#work in progress
+'''#this part of the code was taken from https://www.thepythoncode.com/code/send-receive-files-using-sockets-python
+SEPARATOR = "<SEPARATOR>"
+BUFFER_SIZE = 1024 * 4 #4KB
+
+#define functions
+def send_file(filename, host, port):
+    # get the file size
+    filesize = os.path.getsize(filename)
+    # create the client socket
+    s = socket.socket()
+    print(f"[+] Connecting to {host}:{port}")
+    s.connect((host, port))
+    print("[+] Connected.")
+
+    # send the filename and filesize
+    s.send(f"{filename}{SEPARATOR}{filesize}".encode())
+
+    # start sending the file
+    progress = tqdm.tqdm(range(filesize), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+    with open(filename, "rb") as f:
+        while True:
+            # read the bytes from the file
+            bytes_read = f.read(BUFFER_SIZE)
+            if not bytes_read:
+                # file transmitting is done
+                break
+            # we use sendall to assure transimission in 
+            # busy networks
+            s.sendall(bytes_read)
+            # update the progress bar
+            progress.update(len(bytes_read))
+
+    # close the socket
+    s.close()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Simple File Sender")
+    parser.add_argument("filename", help="File name to send")
+    parser.add_argument("host", help="The host/IP address of the receiver")
+    parser.add_argument("-p", "--port", help="Port to use, default is 5001", default=5001)
+    args = parser.parse_args()
+    filename = args.file
+    host = args.host
+    port = args.port
+    send_file(filename, host, port)'''
